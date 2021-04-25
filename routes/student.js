8b19a47e1d35b115fb3dae431f3cc83f80fa1c3a
vishnuv55 <vishnuviswambharan55@gmail.com>
2021-04-25 15:56:01 +0530

    Add editProfile controller to Rotuer

--- routes/student.js
+++ routes/student.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const { editProfile } = require('../controllers/student');
+const authentication = require('../middlewares/authentication')('student');
 
 const router = express.Router();
 
 router.get('/', () => {});
-router.patch('/', () => {});
+router.patch('/', authentication, editProfile);
 router.post('/login', () => {});
 router.get('/drive-details', () => {});
 router.post('/register-drive', () => {});
