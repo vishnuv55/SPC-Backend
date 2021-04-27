4107c9b1483831ae45e1027526601201e032e7b5
anandhakrishnanm <anandhakrishnanmohank@gmail.com>
2021-04-27 14:57:52 +0530

    add manage bill feature for execom

--- routes/execom.js
+++ routes/execom.js
@@ -3,12 +3,14 @@ const express = require('express');
 const router = express.Router();
 
 const { login } = require('../controllers/execom');
+const { postBillDetails, getBillDetails } = require('../controllers/bill');
+const authentication = require('../middlewares/authentication')('execom');
 
 router.get('/', () => {});
 router.post('/login', login);
 router.get('/drive-details', () => {});
-router.get('/bill-details', () => {});
-router.post('/bill-details', () => {});
+router.get('/bill-details', authentication, getBillDetails);
+router.post('/bill-details', authentication, postBillDetails);
 router.post('send-email', () => {});
 router.post('/update-password', () => {});
 router.post('/logout', () => {});
