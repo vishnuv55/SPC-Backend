0f7336514903f7e61b2d02a8bc10934970fee3c2
Vishnu Viswambharan <vishnuviswambharan55@gmail.com>
2021-05-05 18:52:33 +0530

    Update send mail to send mails by conditionally choosing students (#27)
    * add looger and update console log in errorHandler
    
    * Update sendMail to send mail by conditions & create a few validation functions
    
    * Updated branch array in validation

--- controllers/mail.js
+++ controllers/mail.js
@@ -1,28 +1,72 @@
 const { ErrorHandler } = require('../helpers/error');
 const sendNewMail = require('../helpers/sendNewMail');
-const { validateString } = require('../helpers/validation');
+const {
+  validateString,
+  validateNumber,
+  validateBranchArray,
+  validateGenderArray,
+} = require('../helpers/validation');
 const Student = require('../models/student');
 
 const sendMail = async (req, res, next) => {
   if (req.error) {
     return next(req.error);
   }
+
   // Getting subject and content from req body
-  const { subject, content } = req.body;
+  const {
+    branch_list,
+    number_of_backlogs,
+    tenth_mark,
+    plus_two_mark,
+    btech_cgpa,
+    gender_list,
+    subject,
+    content,
+  } = req.body;
 
   // Validating student details
   try {
+    validateBranchArray(branch_list, 'Branches', false);
+    validateNumber(number_of_backlogs, 0, 40, 'Number of backlogs', false);
+    validateNumber(tenth_mark, 0, 100, '10th mark', false);
+    validateNumber(plus_two_mark, 0, 100, '12th mark', false);
+    validateNumber(btech_cgpa, 0, 20, 'BTech CGPA', false);
+    validateGenderArray(gender_list, 'Gender List', false);
     validateString(subject, 5, 400, 'Subject', true);
     validateString(content, 5, 5000, 'Content', true);
   } catch (error) {
     return next(error);
   }
 
-  // getting emails of all students
-  const emails = await (await Student.distinct('email')).join(', ');
+  // Checking query parameters.
+  const query = {
+    ...(branch_list && { branch: { $in: branch_list } }),
+    ...(number_of_backlogs && { number_of_backlogs: { $lte: number_of_backlogs } }),
+    ...(tenth_mark && { 'tenth_mark.percentage': { $gt: tenth_mark } }),
+    ...(plus_two_mark && { 'plus_two_mark.percentage': { $gt: plus_two_mark } }),
+    ...(btech_cgpa && { btech_cgpa: { $gte: btech_cgpa } }),
+    ...(gender_list && { gender: { $in: gender_list } }),
+  };
+
+  // Finding student that match the query and getting their emails
+  const [{ mail_ids }] = await Student.aggregate([
+    {
+      $match: query,
+    },
+    {
+      $group: {
+        _id: null,
+        mail_ids: { $addToSet: '$email' },
+      },
+    },
+  ]);
+
+  // Joining the mails to a form a string of all emails
+  const mails = mail_ids.join(', ');
 
   // Sending emails to all students
-  const response = await sendNewMail(emails, subject, content);
+  const response = await sendNewMail(mails, subject, content);
 
   // Checking if response is success
   if (!response.success) {
@@ -32,7 +76,7 @@ const sendMail = async (req, res, next) => {
   // Sending success response
   res.status(200).json({
     message: 'Mail has been successfully send',
-    approved: response.approved,
+    accepted: response.accepted,
     rejected: response.rejected,
   });
 };
