b394f6efd1befa88f333af6e308da1d71adcc2b3
vishnuv55 <vishnuviswambharan55@gmail.com>
2021-06-09 11:53:09 +0530

    updated cors url regex

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: /(^https:\/\/cec-spc\.netlify\.app.*)$/,
+    origin: /^https:\/\/cec-spc\.netlify\.app.*/,
     credentials: true,
   })
 );
