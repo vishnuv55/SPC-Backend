01330942ab563826efdb4c049005ff860b15e556
Anandhakrishnan M <57976272+anandhakrishnanm@users.noreply.github.com>
2021-07-11 19:59:11 +0530

    update client url for email (#59)

--- helpers/account-created-alert.js
+++ helpers/account-created-alert.js
@@ -1,6 +1,7 @@
 const CreateAccountMsg = `<h3>Hey there,</h3>
 <p>Your student account is created using this email.</p>
 <p>Your University Register Number will be your password.</p>
-<p>Please visit this link to <a href="https://cec-spc.netlify.app/">login</a>. <br/><br/> If you face any issues in the login, feel free to connect with Placement Cell officials of the college.</p>
+<p>Please visit this link to Login. <br/><br/> <a href="https://chethas.netlify.app">https://chethas.netlify.app/</a> <br/></P> 
+<P> If you face any issues in the login, feel free to connect with Placement Cell officials of the college.</p>
 <p>With Regards,<br />Smart Placement Cell</p>`;
 module.exports = CreateAccountMsg;
